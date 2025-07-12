2bf5eff9c1446a57ed2c284da3ff512fa9abe1da
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2025-07-12 19:11:00 +0700

    change site description

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -2,7 +2,8 @@ import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
 	TITLE: "Roki Miftah Kamaludin",
-	DESCRIPTION: "Roki Miftah Kamaludin personal blog and portfolio.",
+	DESCRIPTION:
+		"A lifelong learner with a passion for keeping up with the latest trends and developments in the ever-evolving field of software engineering.",
 	EMAIL: "me@rokimiftah.id",
 	NUM_POSTS_ON_HOMEPAGE: 2,
 	NUM_PROJECTS_ON_HOMEPAGE: 2,
