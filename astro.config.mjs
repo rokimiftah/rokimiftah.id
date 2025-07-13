a4922ef0b4baaf3deae26a71f6da4e740b08fd5c
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2025-07-13 12:51:34 +0700

    build: change inline css to always

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	build: {
+		inlineStylesheets: "always",
+	},
+
 	devToolbar: {
 		enabled: false,
 	},
