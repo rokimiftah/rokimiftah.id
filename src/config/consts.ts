29b0b91d47cf79e5c68983df1b602e66fff340ec
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2025-11-13 00:34:21 +0700

    change number of posts and projects

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -5,8 +5,8 @@ export const SITE: Site = {
   DESCRIPTION:
     "A lifelong learner with a passion for keeping up with the latest trends and developments in the ever-evolving field of software engineering.",
   EMAIL: "me@rokimiftah.id",
-  NUM_POSTS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 2,
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 5,
 };
 
 export const BLOG: Metadata = {
