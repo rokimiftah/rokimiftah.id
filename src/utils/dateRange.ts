4f64d7d82c7666a4a6e4682503873ba39d9c5455
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2026-04-15 09:08:51 +0700

    chore: remove tailwind config since v4 doesn't need it

--- src/utils/dateRange.ts
+++ src/utils/dateRange.ts
@@ -14,5 +14,5 @@ export function dateRange(startDate: Date, endDate?: Date | string): string {
     }
   }
 
-  return `${startMonth}${startYear} - ${endMonth ?? ""}${endYear ?? ""}`;
+  return `${startMonth} ${startYear} - ${endMonth ?? ""}${endYear ? " " : ""}${endYear ?? ""}`;
 }
